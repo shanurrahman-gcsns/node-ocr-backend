3df461d453f2f8a9bbdcf878479d3e4153743166
shanur <shanur.rahman@gamechangesns.com>
2020-06-22 18:12:53 +0400

    adding constraints specifically for aed cards

--- cloud-vision-nodejs/emiratesIdProcessor.js
+++ cloud-vision-nodejs/emiratesIdProcessor.js
@@ -3,7 +3,7 @@ const parse = require("mrz").parse;
 module.exports = emiratesProcessor = (res, description, initiatePostTimeout) => {
   try {
     let lines;
-    const pattern = /[A-Z]{1}<{1}/;
+    const pattern = /[A-Z]{2}ARE/;
     const startIndex = description.match(pattern).index;
     const untrimmedCode = description
       .slice(startIndex)
@@ -17,7 +17,7 @@ module.exports = emiratesProcessor = (res, description, initiatePostTimeout) => 
 
     lines = mrzCode.match(/.{1,30}/g);
     console.log(mrzCode.length, lines, mrzCode);
-    var result = parse(lines);
+    var result = parse(lines.slice(0, 3));
     return res.status(200).send(result);
   } catch (e) {
     res.status(400).send({error: e.message})
